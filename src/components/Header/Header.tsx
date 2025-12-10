e0a83f92d01fa6a7121a593a65d4f24d85fcb7c5
Leticia Maia <132717041+maia-leticia@users.noreply.github.com>
2025-12-10 16:43:20 -0300

    feat(header): convert to tailwind and improve semantic tags

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,19 +1,21 @@
-import styles from './Header.module.css';
-
 const Header = () => {
+
+  const navLinkStyle = "md:text-sm font-medium text-gray-500 no-underline transition-colors duration-200 hover:text-black text-xs" 
+
   return (
-    <div className={styles.header}>
-      <div className={styles.container}>
-        <div className={styles.logo}>
-          E-TechMerge:
-        </div>
-        <span className={styles.nav}>
-          <a href="#article" className={styles.navLink}>ARTIGOS</a>
-          <a href="#about" className={styles.navLink}>SOBRE NÓS</a>
-        </span>
-      </div>
-    </div>
+    <header className=" h-12 border-b flex justify-center border-b-gray-300">
+      <section className=" mx-auto flex justify-between items-center w-100 md:w-140 xl:w-300 px-4 md:px-20">
+        <h2 className="md:text-lg font-bold color text-4">
+          E-MergingTech
+        </h2>
+        <nav className="flex gap-5">
+          <a href="#article" className={navLinkStyle}>ARTIGOS</a>
+          <a href="#about" className={navLinkStyle}>SOBRE NÓS</a>
+        </nav>
+      </section>
+    </header>
   );
 };
 
 export default Header;
+
