a53f1b65d04d8ef33ec08577b2b11a35d5b2e47b
Leticia Maia <132717041+maia-leticia@users.noreply.github.com>
2025-12-11 15:19:26 -0300

    feat(Footer): convert to tailwind and semantics

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,12 +1,10 @@
-import styles from './Footer.module.css';
-
 const Footer = () => {
   return (
-    <div className={styles.footer}>
-      <div className={styles.container}>
-        <span className={styles.copyright}>© 2023 Copyright. Emerging Tech</span>
+    <footer className="bg-[#1f2937] md:py-2 py-6">
+      <div className="max-w-7xl my-0 mx-auto md:px-8 px-4 text-center ">
+        <p className="md:text-[14px] text-sm text-[#9ca3af]">© 2023 Copyright. Emerging Tech</p>
       </div>
-    </div>
+    </footer>
   );
 };
 
